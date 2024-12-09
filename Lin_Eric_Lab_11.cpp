250107ff33b60a9642f556ad9de427aea1d3924b
ericl343 <ericl343@nycstudents.net>
2024-12-09 02:50:34 -0500

    11

--- Lin_Eric_Lab_11.cpp
+++ Lin_Eric_Lab_11.cpp
@@ -11,80 +11,78 @@
 
 using namespace std;
 
-//Void functions
-const int ARRAY = 50; 
-void iniArray(double alpha[]); 
-void printArray (double alpha[]);
-void fillRandom(double alpha[]);
-//Variables
-double computeAver(double alpha[]); 
-int count100(double alpha[]); 
+//Main Code
+int main () {
+    const int ARRAY_SIZE = 50;                  //Size of the array
+    double alpha[ARRAY_SIZE];
 
-//Start of code
-int main() { double alpha[ARRAY]; 
-
-//Initialize array with specific values 
-iniArray(alpha); cout << "Array after initialization: " << endl; 
-printArray(alpha); 
-//Fill array with random numbers from 1-100
-fillRandom(alpha); cout << "Array after filling with random values: " << endl;  
-printArray(alpha); 
-//Print the values in the array
-double average = computeAver(alpha); 
-cout << "\nAverage of array elements: " << average << endl; 
-//Count elements exactly equal to 100 
-int count = count100(alpha); 
-cout << "Number of elements equal to 100: " << count << endl; 
-return 0; 
-} 
-//Initialize the array as described 
-void iniArray(double alpha[]) { 
-    for (int i = 0; i < 25; i++) 
-        {alpha[i] = i * i;} 
-    for (int i = 25; i < ARRAY; i++) 
-        {alpha[i] = i * 3;} 
-} 
-//Print the array with 10 elements per line 
-void printArray(double alpha[]) { 
-    for (int i = 0; i < ARRAY; i++) {
-    cout << setw(6) << alpha[i]; 
-    if ((i + 1) % 10 == 0) {cout << endl;}}
-} 
-//Fill array with random numbers between 1 and 100 
-void fillRandom(double alpha[]) { 
-    srand(static_cast<unsigned int>(time(0))); 
-//Random number generator 
-for (int i = 0; i < ARRAY; i++)  
-    {alpha[i] = rand() % 100 + 1;} 
-} 
-//Compute and return the average of array elements 
-double computeAver(double alpha[]) { 
-    double sum = 0.0; 
-    for (int i = 0; i < ARRAY; i++)
-    {sum += alpha[i];} 
-    return sum / ARRAY; 
-} 
-//Count and return the number of elements exactly equal to 100 
-int count100(double alpha[]) { 
-    int count = 0; 
-    for (int i = 0; i < ARRAY; i++) {
-        if (alpha[i] == 100) {count++;}} 
-    return count;
+//Part 1
+cout << "Part 1 and 2" << endl;
+for (int i = 0; i < ARRAY_SIZE; i++) {
+    if (i < ARRAY_SIZE / 2) {               //Divides into half so before 25
+        alpha[i] = pow(i, 2);               
+    } else {
+        alpha[i] = i * 3;                   //Multiples of 3
+        }
+    }
+//Part 2
+for (int i = 0; i < ARRAY_SIZE; i++) {
+    cout << setw(5) << alpha[i] << " ";     //Setting a fixed width
+    if ((i + 1) % 10 == 0) {                //Print a new line every 10 elements
+        cout << endl;
+        }
+    }
+//Part 3
+cout << "\nPart 3" << endl;
+srand(time(0));                                 //RNG using time
+for (int i = 0; i < ARRAY_SIZE; i++) {
+    alpha[i] = 1 + rand() % 100;                //RNG Between 1-100
+    cout << setw(6) << alpha[i] << " ";
+    if ((i + 1) % 10 == 0) {                    //Still 10 per line
+        cout << endl;
+    }
 }
-/*
-Array after initialization: 
-     0     1     4     9    16    25    36    49    64    81
-   100   121   144   169   196   225   256   289   324   361
-   400   441   484   529   576    75    78    81    84    87
-    90    93    96    99   102   105   108   111   114   117
-   120   123   126   129   132   135   138   141   144   147
-Array after filling with random values:
-    84    34    63    17    66    66    57    45    98    75
-    11    49    72     3    31    89    88    72    26    68
-    44    49   100    13     5    66    95    95    13    80
-    93    50    97    91    55    30    62     3    81     3
-    33    88    42    26    67     6    46    99    80    31
+//Part 4
+cout << "\nPart 4" << endl;
+double average;
+double sum = 0;
+for (int i = 0; i < ARRAY_SIZE; i++) {          //Loop through array
+    sum += alpha[i];
+    }
+    average = sum / ARRAY_SIZE;                 //Divide total by array size for average
+    cout << "The average is : " << average << endl;
 
-Average of array elements: 55.14
-Number of elements equal to 100: 1
+//Part 5
+cout << "\nPart 5" << endl;
+int ele100 = 0;                                 //Starting amount of elements that are 100
+for (int i = 0; i < ARRAY_SIZE; i++) {          //Loop through array
+    if (alpha[i] == 100) {                      //Check if value is 100
+        ele100++;                               //If value is 100, then add to count
+        }
+    }
+    cout << "Number of elements equal to 100 : " << ele100 << endl;
+
+ return 0;                                      //End of my program
+}
+
+/*
+Part 1 and 2
+    0     1     4     9    16    25    36    49    64    81
+  100   121   144   169   196   225   256   289   324   361
+  400   441   484   529   576    75    78    81    84    87
+   90    93    96    99   102   105   108   111   114   117
+  120   123   126   129   132   135   138   141   144   147
+
+Part 3
+    77     79     86     29     89     80     53     94     87     93
+    33     11     89     96     27     80     87     52     94     57
+    38     46     98     52      9     37     46     76      7     59
+     7     41     38     84     43     62      4     85     79     31
+    64     25     41     57     35     73     41     18     79     70
+
+Part 4
+The average is : 56.76
+
+Part 5
+Number of elements equal to 100 : 0
 */
